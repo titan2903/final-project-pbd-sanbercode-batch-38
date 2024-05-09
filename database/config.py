5fb6ef9f9ad5f9474bdeed9bbf1fac3749f21f5f
titan <titanioy98@gmail.com>
2024-05-09 11:22:36 +0700

    fix: remove logs

--- database/config.py
+++ database/config.py
@@ -7,12 +7,8 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_CONN = config("DATABASE_CONN")
 
-print("SQLALCHEMY_DATABASE_CONN", SQLALCHEMY_DATABASE_CONN)
+engine = create_engine(SQLALCHEMY_DATABASE_CONN, echo=True)
 
-engine=create_engine(SQLALCHEMY_DATABASE_CONN,
-    echo=True
-)
+Base = declarative_base()
 
-Base=declarative_base()
-
-SessionLocal=sessionmaker(bind=engine)
+SessionLocal = sessionmaker(bind=engine)
