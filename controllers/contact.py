235af16d1f8b19dadcc3c584edd1af6edcaa7375
titan <titanioy98@gmail.com>
2024-05-13 14:22:10 +0700

    fix: indetation and remove py cache folder

--- controllers/contact.py
+++ controllers/contact.py
@@ -8,53 +8,59 @@ from entity.ContactTransformer import singleTransformContact
 from utils.validate_email import validateEmail
 from middleware.auth_handler import get_current_user
 
-db=SessionLocal()
+db = SessionLocal()
+
 
 class ContactController:
     @staticmethod
     async def store(request: Request) -> JSONResponse:
         try:
-            auth_token = request.headers.get('Authorization')
-            token = auth_token.split('Bearer')[1].strip()
+            auth_token = request.headers.get("Authorization")
+            token = auth_token.split("Bearer")[1].strip()
             userId = get_current_user(token)
-            
+
             body = await request.json()
-            contact_name = body['contact_name']
-            phone = body['phone']
-            email = body['email']
-            
+            contact_name = body["contact_name"]
+            phone = body["phone"]
+            email = body["email"]
+
             if contact_name == "":
                 raise Exception("contact name couldn't be empty!")
             elif email == "":
                 raise Exception("email can't be empty!")
             elif phone == "":
                 raise Exception("phone can't be empty!")
-            
+
             isEmailValid = validateEmail(email)
-            
+
             if isEmailValid == False:
                 raise Exception("Invalid Email!")
-            
-            contact = Contacts(customer_id=userId, contact_name=contact_name, phone=phone, email=email.lower())
+
+            contact = Contacts(
+                customer_id=userId,
+                contact_name=contact_name,
+                phone=phone,
+                email=email.lower(),
+            )
             db.add(contact)
             db.commit()
             transformer = singleTransformContact(contact)
             return ok(transformer, "Success Create Contact!")
         except Exception as e:
             db.rollback()
-            return badRequest('', f'{e}')
-    
+            return badRequest("", f"{e}")
+
     @staticmethod
     async def update(id: int, request: Request) -> JSONResponse:
         try:
-            auth_token = request.headers.get('Authorization')
-            token = auth_token.split('Bearer')[1].strip()
+            auth_token = request.headers.get("Authorization")
+            token = auth_token.split("Bearer")[1].strip()
             userId = get_current_user(token)
-            
+
             body = await request.json()
-            contact_name = body['contact_name']
-            phone = body['phone']
-            email = body['email']
+            contact_name = body["contact_name"]
+            phone = body["phone"]
+            email = body["email"]
 
             if contact_name == "":
                 raise Exception("contact name couldn't be empty!")
@@ -63,49 +69,46 @@ class ContactController:
             elif phone == "":
                 raise Exception("phone can't be empty!")
 
-
             isEmailValid = validateEmail(email)
-            
+
             if isEmailValid == False:
                 raise Exception("Invalid Email!")
-            
-            contactUpdate = db.query(Contacts).filter(Contacts.contact_id==id).first()
+
+            contactUpdate = db.query(Contacts).filter(Contacts.contact_id == id).first()
             if contactUpdate is None:
-                raise Exception('contact not found!')
+                raise Exception("contact not found!")
 
             contactUpdate.contact_name = contact_name
             contactUpdate.phone = phone
             contactUpdate.email = email.lower()
-        
 
             transformer = singleTransformContact(contactUpdate)
             if transformer["customer_id"] != userId:
-                raise Exception('You are not Owner!')
-            
+                raise Exception("You are not Owner!")
+
             db.commit()
             return ok(transformer, "Success Update Contact!")
         except Exception as e:
             db.rollback()
-            return badRequest('', f'{e}')
+            return badRequest("", f"{e}")
 
     @staticmethod
-    async def getById(id:int, request: Request) -> JSONResponse:
+    async def getById(id: int, request: Request) -> JSONResponse:
         try:
-            auth_token = request.headers.get('Authorization')
-            token = auth_token.split('Bearer')[1].strip()
+            auth_token = request.headers.get("Authorization")
+            token = auth_token.split("Bearer")[1].strip()
             userId = get_current_user(token)
 
-            contact = db.query(Contacts).filter(Contacts.contact_id==id).first()
-            
+            contact = db.query(Contacts).filter(Contacts.contact_id == id).first()
 
             if contact is None:
-                raise Exception('Contact not Found!')
+                raise Exception("Contact not Found!")
 
             transformer = singleTransformContact(contact)
 
             if transformer["customer_id"] != userId:
-                raise Exception('You are not Owner!')
-            
+                raise Exception("You are not Owner!")
+
             return ok(transformer, "Success Get Contact!")
         except Exception as e:
-            return badRequest('', f'{e}')
+            return badRequest("", f"{e}")
